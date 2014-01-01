f5a6da9df833777ade089d6f1b2a7b5d3059e503
Stanislaw Pankevich <s.pankevich@gmail.com>
2014-01-02 02:13:18 +0400

    Clean-up NSString/Concatenation

--- FoundationExtensions/NSString/Concatenation.h
+++ FoundationExtensions/NSString/Concatenation.h
@@ -4,18 +4,11 @@
 //
 // Copyright (c) 2013 Stanislaw Pankevich
 // Released under the MIT license
-//
-// http://stackoverflow.com/a/9730316/598057
 
 #import <Foundation/NSString.h>
 
 @interface NSString (Concatenation)
 
-- (NSString *):(NSString *)a;
-- (NSString *):(NSString *)a :(NSString *)b;
-- (NSString *):(NSString *)a :(NSString *)b :(NSString *)c;
-- (NSString *):(NSString *)a :(NSString *)b :(NSString *)c :(NSString *)d;
-
-- (NSString *)concat:(NSString *)strings, ...;
+- (NSString *)concatenateWithStrings:(NSString *)strings, ...;
 
 @end
--- FoundationExtensions/NSString/Concatenation.m
+++ FoundationExtensions/NSString/Concatenation.m
@@ -11,34 +11,19 @@
 
 @implementation NSString (Concatenation)
 
-- (NSString *):(NSString *)a {
-    return [self stringByAppendingString:a];
-}
-
-- (NSString *):(NSString *)a :(NSString *)b {
-    return [[self:a]:b];
-}
-
-- (NSString *):(NSString *)a :(NSString *)b :(NSString *)c {
-    return [[[self:a]:b]:c];
-}
-
-- (NSString *):(NSString *)a :(NSString *)b :(NSString *)c :(NSString *)d {
-    return [[[[self:a]:b]:c]:d];
-}
-
-- (NSString *)concat:(NSString *)strings, ... {
+- (NSString *)concatenateWithStrings:(NSString *)strings, ... {
     va_list args;
     va_start(args, strings);
 
     NSString *s;
-    NSString *con = [self stringByAppendingString:strings];
+    NSString *concatenatedString = [self stringByAppendingString:strings];
 
     while((s = va_arg(args, NSString *)))
-        con = [con stringByAppendingString:s];
+        concatenatedString = [concatenatedString stringByAppendingString:s];
 
     va_end(args);
-    return con;
+
+    return concatenatedString;
 }
 
 @end
